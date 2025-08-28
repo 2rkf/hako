d961e914581931512f4d7ba20907e5065a6e82a5
Reina <notmarx.tech@gmail.com>
2025-08-28 20:21:26 +0700

    feat(bbcode): Add recursive parsing with multiline support

--- server/utils/bbcode.ts
+++ server/utils/bbcode.ts
@@ -1,13 +1,17 @@
 export function parseBBCode(text: string): string {
-    return text
-        .replace(/\[b\](.*?)\[\/b\]/gi, "<strong>$1</strong>")
-        .replace(/\[i\](.*?)\[\/i\]/gi, "<em>$1</em>")
-        .replace(/\[u\](.*?)\[\/u\]/gi, "<u>$1</u>")
-        .replace(/\[s\](.*?)\[\/s\]/gi, "<s>$1</s>")
-        .replace(/\[code\](.*?)\[\/code\]/gis, `<code class="bg-midnight-100 dark:bg-midnight-800 px-1 rounded">$1</code>`)
-        .replace(/\[url=(.*?)\](.*?)\[\/url\]/gi, `<a href="$1" target="_blank" class="text-secondary hover:underline">$2</a>`)
-        .replace(/\[quote\](.*?)\[\/quote\]/gis, `<blockquote class="border-l-4 border-gray-400 pl-4 italic opacity-80">$1</blockquote>`)
-        .replace(/\[spoiler\](.*?)\[\/spoiler\]/gi, `<span class="bg-gray-800 text-gray-800 hover:text-white transition-colors px-1 rounded cursor-help">$1</span>`)
-        .replace(/\[br\]/gi, "<br>")
-        .replace(/\n/g, "<br>");
+    function parse(input: string): string {
+        return input
+            .replace(/\[b\](.*?)\[\/b\]/gis, (_, inner) => `<strong>${parse(inner)}</strong>`)
+            .replace(/\[i\](.*?)\[\/i\]/gis, (_, inner) => `<em>${parse(inner)}</em>`)
+            .replace(/\[u\](.*?)\[\/u\]/gis, (_, inner) => `<u>${parse(inner)}</u>`)
+            .replace(/\[s\](.*?)\[\/s\]/gis, (_, inner) => `<s>${parse(inner)}</s>`)
+            .replace(/\[code\](.*?)\[\/code\]/gis, (_, inner) => `<code class="bg-midnight-100 dark:bg-midnight-800 px-1 rounded">${inner}</code>`)
+            .replace(/\[url=(.*?)\](.*?)\[\/url\]/gis, (_, href, inner) => `<a href="${href}" target="_blank" class="text-secondary hover:underline">${parse(inner)}</a>`)
+            .replace(/\[quote\](.*?)\[\/quote\]/gis, (_, inner) => `<blockquote class="border-l-4 border-gray-400 pl-4 italic opacity-80">${parse(inner)}</blockquote>`)
+            .replace(/\[spoiler\](.*?)\[\/spoiler\]/gis, (_, inner) => `<span class="bg-gray-800 text-gray-800 hover:text-white transition-colors px-1 rounded cursor-help">${parse(inner)}</span>`)
+            .replace(/\[br\]/gi, "<br>")
+            .replace(/\n/g, "<br>");
+    }
+
+    return parse(text);
 }
