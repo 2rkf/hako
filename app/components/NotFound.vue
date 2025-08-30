c42171f0fffbe92a80d5721e5deeb66b63853c37
Reina <notmarx.tech@gmail.com>
2025-08-30 14:03:45 +0700

    feat(components): Fix padding positions

--- app/components/NotFound.vue
+++ app/components/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center text-center py-16">
+  <div class="flex flex-col items-center text-center py-24">
     <h1 class="text-5xl font-bold text-red-500 mb-4">404</h1>
     <p class="text-lg text-gray-600 dark:text-gray-400 mb-6">
       {{ $t("notfound.desc") }}
--- app/pages/[404].vue
+++ app/pages/[404].vue
@@ -1,3 +1,9 @@
 <template>
-    <NotFound />
+  <div
+    class="min-h-screen bg-midnight-100 dark:bg-midnight-950 transition-colors font-sans noselect"
+  >
+    <div class="max-w-4xl mx-auto px-4 pt-16 pb-6">
+      <NotFound />
+    </div>
+  </div>
 </template>
