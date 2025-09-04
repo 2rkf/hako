7d1fa237d891f8946ac29f0fdb13b2da2680e633
Reina <notmarx.tech@gmail.com>
2025-09-05 06:00:10 +0700

    feat(tab): Improve tab menu

--- app/components/Tab.vue
+++ app/components/Tab.vue
@@ -1,28 +1,31 @@
 <script setup>
 const { locale, locales, setLocale } = useI18n();
-
-const availableLocales = computed(() => {
-  return locales.value.filter((i) => i.code !== locale.value);
-});
 </script>
 
 <template>
-  <div
-    class="absolute top-4 right-4 sm:top-8 sm:right-10 flex items-center space-x-1"
-  >
-    <!-- Colour Mode Switcher -->
-    <UColorModeButton />
-    
-    <!-- Language Switcher -->
-    <div v-for="locale in availableLocales" :key="locale.code">
+  <div class="absolute right-4 sm:right-12 top-4 sm:top-8">
+    <UPopover>
       <UButton
-        class="text-brick-red-500 dark:text-brick-red-200 noselect font-semibold text-lg sm:text-xl"
+        icon="i-lucide-menu"
+        :label="$t('menu')"
+        size="xl"
         color="neutral"
         variant="ghost"
-        @click="setLocale(locale.code)"
-      >
-        {{ locale.name }}
-      </UButton>
-    </div>
+        class="text-brick-red-500 dark:text-brick-red-200"
+      />
+
+      <template #content>
+        <div
+          class="flex flex-col items-center justify-center space-y-3 p-3 min-w-[160px]"
+        >
+          <UColorModeSwitch />
+          <ULocaleSelect
+            v-model="locale"
+            :locales="locales"
+            @update:model-value="setLocale($event)"
+          />
+        </div>
+      </template>
+    </UPopover>
   </div>
 </template>
